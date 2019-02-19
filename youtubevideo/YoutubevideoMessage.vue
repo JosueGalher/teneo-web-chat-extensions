d4f08a719a99643d3742fb0c4c708d0d14aa92db
Lucas Willering <lucas.willering@artificial-solutions.com>
2019-02-19 10:05:02 +0100

    first commit

---
+++ youtubevideo/YoutubevideoMessage.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="video-message">
+    <style>.embed-container { position: relative; padding-bottom: 56.25%; height: 0; overflow: hidden; max-width: 100%; } .embed-container iframe, .embed-container object, .embed-container embed { position: absolute; top: 0; left: 0; width: 100%; height: 100%; }</style>
+    <div class='embed-container'>
+      <iframe :src="videoUrl" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'YoutubevideoMessage',
+  props: {
+    message: {
+      type: Object,
+      required: true,
+      validator: (message) => {
+        return (
+          message &&
+          message.type === 'youtubevideo' &&
+          message.data &&
+          message.data.payload &&
+          message.data.payload.url
+        );
+      },
+    },
+  },
+  computed: {
+    videoUrl() {
+      return this.message.data.payload.url;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.video-message img {
+  margin: 12px;
+  max-width: 90%;
+}
+</style>
